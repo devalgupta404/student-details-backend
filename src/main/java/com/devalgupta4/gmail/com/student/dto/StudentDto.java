f372234f5eec0c1d85ee5412e588d0a74f11398b
Deval Gupta <devalgupta4@gmail.com>
2025-09-12 22:22:57 +0530

    making dto

---
+++ src/main/java/com/devalgupta4/gmail/com/student/dto/StudentDto.java
@@ -0,0 +1,15 @@
+package com.devalgupta4.gmail.com.student.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+ @Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto {
+    private Long id;
+    private String name;
+    private String email;
+}
+

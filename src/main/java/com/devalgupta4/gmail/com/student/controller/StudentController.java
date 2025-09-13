f3f6f771970c6e2fafd19ad0e95f4a9d24821950
Deval Gupta <devalgupta4@gmail.com>
2025-09-13 18:18:21 +0530

    changing routes

--- src/main/java/com/devalgupta4/gmail/com/student/controller/StudentController.java
+++ src/main/java/com/devalgupta4/gmail/com/student/controller/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("/student")
 public class StudentController {
     private final StudentService studentService;
 

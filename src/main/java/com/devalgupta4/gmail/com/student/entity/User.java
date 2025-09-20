bcc7cbf59e600e07b886cd940c7572a1c3e9d5b5
Deval Gupta <devalgupta4@gmail.com>
2025-09-20 23:36:25 +0530

    making user entity

---
+++ src/main/java/com/devalgupta4/gmail/com/student/entity/User.java
@@ -0,0 +1,41 @@
+package com.devalgupta4.gmail.com.student.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String collegeDomain;
+
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Item> items;
+
+    // Constructor for easy creation
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.collegeDomain = extractCollegeDomain(email);
+    }
+
+    private String extractCollegeDomain(String email) {
+        if (email != null && email.contains("@")) {
+            return email.substring(email.indexOf("@") + 1);
+        }
+        return null;
+    }
+}
